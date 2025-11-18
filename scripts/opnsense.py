c962bac9440f0511b2dbe1131ee18a86624d3726
e.j. (they/them) <git@ejs.sh>
2025-11-18 10:18:01 -0800

    reuse annotate to update licenses

--- scripts/opnsense.py
+++ scripts/opnsense.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2025 e.j. sahala <ej@saha.la>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 #
 #
 
--- src/web/main.go
+++ src/web/main.go
@@ -1,1 +1,5 @@
+// SPDX-FileCopyrightText: 2025 e.j. sahala <ej@saha.la>
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package web
