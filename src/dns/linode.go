ec6e49d11bfba7610e0eefb4a1cdbb8eb10785b5
e.j. (they/them) <git@ejs.sh>
2025-11-18 22:01:13 -0800

    refactoring first linode imports

--- src/dns/linode.go
+++ src/dns/linode.go
@@ -5,61 +5,41 @@
 package dns
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/pulumi/pulumi-linode/sdk/v5/go/linode"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
+var (
+	// LinodeDomains is a list of domains to be created in Linode DNS.
+	linodeDomains = []Domain{
+		{Name: "sahala.org"},
+		{Name: "saha.la"},
+		{Name: "ejs.sh"},
+		{Name: "ejs.wtf"},
+		{Name: "octopik3s.io"},
+	}
+)
+
 func manageLinodeDNS(ctx *pulumi.Context) error {
-	// Replace dots with hyphens for resource naming
-	// resourceName := fmt.Sprintf("domain-%s", strings.ReplaceAll(domainName, ".", "-"))
-	_, err := linode.NewDomain(ctx, "domain-sahala-org", &linode.DomainArgs{
-		Domain:   pulumi.String("sahala.org"),
-		SoaEmail: pulumi.String("domains@sahala.org"),
-		Status:   pulumi.String("active"),
-		Type:     pulumi.String("master"),
-	}, pulumi.Protect(true))
-	if err != nil {
-		return err
-	}
-
-	_, err = linode.NewDomain(ctx, "domain-saha-la", &linode.DomainArgs{
-		Domain:   pulumi.String("saha.la"),
-		SoaEmail: pulumi.String("domains@saha.la"),
-		Status:   pulumi.String("active"),
-		Type:     pulumi.String("master"),
-	}, pulumi.Protect(true))
-	if err != nil {
-		return err
-	}
-
-	_, err = linode.NewDomain(ctx, "domain-ejs-sh", &linode.DomainArgs{
-		Domain:   pulumi.String("ejs.sh"),
-		SoaEmail: pulumi.String("domains@ejs.sh"),
-		Status:   pulumi.String("active"),
-		Type:     pulumi.String("master"),
-	}, pulumi.Protect(true))
-	if err != nil {
-		return err
-	}
-
-	_, err = linode.NewDomain(ctx, "domain-ejs-wtf", &linode.DomainArgs{
-		Domain:   pulumi.String("ejs.wtf"),
-		SoaEmail: pulumi.String("domains@ejs.wtf"),
-		Status:   pulumi.String("active"),
-		Type:     pulumi.String("master"),
-	}, pulumi.Protect(true))
-	if err != nil {
-		return err
-	}
-
-	_, err = linode.NewDomain(ctx, "domain-octopik3s-io", &linode.DomainArgs{
-		Domain:   pulumi.String("octopik3s.io"),
-		SoaEmail: pulumi.String("hostmaster@octopik3s.io"),
-		Status:   pulumi.String("active"),
-		Type:     pulumi.String("master"),
-	}, pulumi.Protect(true))
-	if err != nil {
-		return err
+	for _, domain := range linodeDomains {
+		// Replace dots with hyphens for resource naming
+		resourceName := fmt.Sprintf("domain-%s", strings.ReplaceAll(domain.Name, ".", "-"))
+		soaEmail := fmt.Sprintf("domains@%s", domain.Name)
+		if domain.Name == "octopik3s.io" {
+			soaEmail = fmt.Sprintf("hostmaster@%s", domain.Name)
+		}
+		_, err := linode.NewDomain(ctx, resourceName, &linode.DomainArgs{
+			Domain:   pulumi.String(domain.Name),
+			SoaEmail: pulumi.String(soaEmail),
+			Status:   pulumi.String("active"),
+			Type:     pulumi.String("master"),
+		}, pulumi.Protect(true))
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
